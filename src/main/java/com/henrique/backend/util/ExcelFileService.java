42823bc2fd284e41b15f2371ec73f04de8af30a7
henrique brendon <henriquebrendonti26@gmail.com>
2024-11-14 15:35:16 -0300

    Functionality to find out how many rows contain data on each sheet

--- src/main/java/com/henrique/backend/util/ExcelFileService.java
+++ src/main/java/com/henrique/backend/util/ExcelFileService.java
@@ -2,6 +2,9 @@ package com.henrique.backend.util;
 
 import java.io.FileInputStream;
 
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
@@ -22,9 +25,36 @@ public final class ExcelFileService {
             var book = new XSSFWorkbook(file);
             for (int i = 0; i < book.getNumberOfSheets(); i++) {
                 sheet = book.getSheetAt(i);
+                System.out.println(filledLines(sheet));
             }
         } catch (Exception e) {
             e.getMessage();
         }
+    }
+
+     // Function to find out the rows that contain data
+    int filledLines(XSSFSheet sheet) {
+        int filledRowsCount = 0;
+
+        // Iterate over all rows up to the last row with content
+        for (int i = 0; i <= sheet.getLastRowNum(); i++) {
+            XSSFRow currentRow = sheet.getRow(i);
+            if (currentRow != null) {
+                boolean isRowFilled = false;
+                for (int j = 0; j < ROW_NUMBER; j++) {
+                    XSSFCell cell = currentRow.getCell(j);
+                    if (cell != null && cell.getCellType() != CellType.BLANK) {
+                        isRowFilled = true;
+                        break;
+                    }
+                }
+                if (isRowFilled) {
+                    filledRowsCount++;
+                } else if (filledRowsCount > 0) {
+                    break;
+                }
+            }
+        }
+        return filledRowsCount;
     }
 }
