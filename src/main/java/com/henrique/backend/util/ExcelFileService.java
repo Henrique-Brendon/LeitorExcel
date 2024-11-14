ba53b0458a819034415055d74c264f2e77284aeb
henrique brendon <henriquebrendonti26@gmail.com>
2024-11-14 15:39:13 -0300

    Functionality to read and capture the data contained in each sheet

--- src/main/java/com/henrique/backend/util/ExcelFileService.java
+++ src/main/java/com/henrique/backend/util/ExcelFileService.java
@@ -25,7 +25,7 @@ public final class ExcelFileService {
             var book = new XSSFWorkbook(file);
             for (int i = 0; i < book.getNumberOfSheets(); i++) {
                 sheet = book.getSheetAt(i);
-                System.out.println(filledLines(sheet));
+                verticalPath(sheet, filledLines(sheet) - HEADER_NUMBER);
             }
         } catch (Exception e) {
             e.getMessage();
@@ -33,7 +33,7 @@ public final class ExcelFileService {
     }
 
      // Function to find out the rows that contain data
-    int filledLines(XSSFSheet sheet) {
+    private int filledLines(XSSFSheet sheet) {
         int filledRowsCount = 0;
 
         // Iterate over all rows up to the last row with content
@@ -57,4 +57,36 @@ public final class ExcelFileService {
         }
         return filledRowsCount;
     }
+
+    // Procedure to read data from the sheet
+    private void verticalPath(XSSFSheet sheet, int lastRow) {
+        int count = HEADER_NUMBER;
+
+        for (int vertical = 0; vertical < lastRow; vertical++) {
+            XSSFRow row = sheet.getRow(count);
+            if (row != null) {
+                readData(row);
+            }
+            count++;
+        }
+    }
+    
+    private void readData(XSSFRow row) {
+        String[] attributes = new String[ROW_NUMBER];
+        for (int i = 0; i < ROW_NUMBER; i++) {
+            XSSFCell cell = row.getCell(i);
+            System.out.println(printCellValue(cell));
+        }
+    }
+
+    private String printCellValue(XSSFCell cell) {
+        return switch (cell.getCellType()) {
+            case STRING -> cell.getStringCellValue();
+            case NUMERIC -> String.valueOf(cell.getNumericCellValue());
+            case BOOLEAN -> String.valueOf(cell.getBooleanCellValue());
+            case FORMULA -> cell.getCellFormula();
+            default -> "Type not supported";
+        };
+    }
+    
 }
