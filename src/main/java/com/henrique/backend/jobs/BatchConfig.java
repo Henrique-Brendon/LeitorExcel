e8bd54545edc0356efd742c37317a19074c0106f
henrique brendon <henriquebrendonti26@gmail.com>
2024-11-16 15:20:21 -0300

    Create job

---
+++ src/main/java/com/henrique/backend/jobs/BatchConfig.java
@@ -0,0 +1,29 @@
+package com.henrique.backend.jobs;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+public class BatchConfig {
+    private final PlatformTransactionManager transactionManager;
+    private final JobRepository jobRepository;
+
+    public BatchConfig(PlatformTransactionManager transactionManager, JobRepository jobRepository) {
+        this.transactionManager = transactionManager;
+        this.jobRepository = jobRepository;
+    }
+
+    @Bean
+    Job jobExcelProcessing(Step stepExcelFile) {
+        return new JobBuilder("jobExcelProcessing", jobRepository)
+            .incrementer(new RunIdIncrementer())
+            .start(stepExcelFile)
+            .build();
+    }
+}
