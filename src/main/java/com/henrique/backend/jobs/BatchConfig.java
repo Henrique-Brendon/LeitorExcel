a334bb5ff0cf4e6fc4097255c5ac766ffd4dfaf8
henrique brendon <henriquebrendonti26@gmail.com>
2024-11-16 15:28:59 -0300

    Create step

--- src/main/java/com/henrique/backend/jobs/BatchConfig.java
+++ src/main/java/com/henrique/backend/jobs/BatchConfig.java
@@ -1,13 +1,20 @@
 package com.henrique.backend.jobs;
 
+import org.apache.commons.math3.stat.descriptive.summary.Product;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
+
+import com.henrique.backend.dtos.ProductDTO;
 
 @Configuration
 public class BatchConfig {
@@ -24,6 +31,16 @@ public class BatchConfig {
         return new JobBuilder("jobExcelProcessing", jobRepository)
             .incrementer(new RunIdIncrementer())
             .start(stepExcelFile)
+            .build();
+    }
+
+    @Bean
+    Step stepExcelFile(ItemReader<ProductDTO> reader, ItemProcessor<ProductDTO, Product> processor, ItemWriter<Product> writer) {
+        return new StepBuilder("stepExcelFile", jobRepository)
+            .<ProductDTO, Product>chunk(1000, transactionManager)
+            .reader(reader)
+            .processor(processor)
+            .writer(writer)
             .build();
     }
 }
