6b780e14cce18acd2152a70a06336803947fa334
henrique brendon <henriquebrendonti26@gmail.com>
2024-12-14 10:05:36 -0300

    Feat: add ProductController with CRUD operations

---
+++ src/main/java/com/henrique/backend/controllers/ProductController.java
@@ -0,0 +1,69 @@
+package com.henrique.backend.controllers;
+
+import com.henrique.backend.dtos.ProductDTO;
+import com.henrique.backend.entities.Product;
+import com.henrique.backend.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping()
+public class ProductController {
+    
+    @Autowired
+    private ProductService productService;
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> findById(@PathVariable long id) {
+        Product product = productService.findById(id);
+        return ResponseEntity.ok().body(product);
+    }
+    @GetMapping("/products")
+    public ResponseEntity<List<ProductDTO>> getAllProducts() {
+        List<ProductDTO> products = productService.findAll();
+        return ResponseEntity.ok(products);
+    }
+    
+
+    @GetMapping("/filteredProducts")
+    public ResponseEntity<List<ProductDTO>> getAllProductsSort(@RequestParam String campo, @RequestParam String direction) {
+        List<ProductDTO> products = productService.getAllProductsSort(campo, direction);
+        return ResponseEntity.ok(products);
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> insert(@RequestBody ProductDTO productDTO) {
+        productService.insert(productDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PostMapping("/bulk-insert")
+    public ResponseEntity<Void> insertProducts(@RequestBody List<ProductDTO> productDTOs) {
+        productService.insertProducts(productDTOs);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable Long id, @RequestBody ProductDTO productDTO) {
+        Product updatedProduct = productService.update(id, productDTO);
+        return ResponseEntity.ok(updatedProduct);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        productService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+}
